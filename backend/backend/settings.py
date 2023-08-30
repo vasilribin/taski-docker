aad7704925675912d292e205fc20dc91d6cc82eb
Рыбин Василий <vasil.ribin@ya.ru>
2023-08-30 19:37:37 +0700

    Add workflow with PEP8 linting -01

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,19 +60,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': '/data/db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': {
